e4e767265b8d28c25c7114fe4104d745c262c338
s24861597-stella <s24861597@gmail.com>
2026-05-28 11:42:49 +0800

    更新 main.py

--- discord-bot/main.py
+++ discord-bot/main.py
@@ -15,8 +15,8 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 PREFIX = os.getenv("COMMAND_PREFIX", "!")
 
 intents = discord.Intents.default()
-
 intents.message_content = True
+intents.members = True
 
 bot = commands.Bot(command_prefix=PREFIX, intents=intents, help_command=None)
 
